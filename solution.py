0fef06222b8cda4ecc8cca5ca67b508076753c9c
Akhil <coolakhil.123@gmail.com>
2017-02-23 05:29:04 +0530

    implement diagonal units strategy

--- solution.py
+++ solution.py
@@ -6,7 +6,6 @@ assignments = []
 def cross(a, b):
     return [s + t for s in a for t in b]
 
-
 rows = 'ABCDEFGHI'
 cols = '123456789'
 
@@ -15,7 +14,13 @@ boxes = cross(rows, cols)
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
-unitlist = row_units + column_units + square_units
+
+#add diagonal units
+diagonal_units = [[x+y for x,y in zip(rows, cols)]]
+rev_diagonal_units = [[x+y for x,y in zip(rows, cols[::-1])]]
+
+
+unitlist = row_units + column_units + square_units + diagonal_units + rev_diagonal_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s], [])) - set([s])) for s in boxes)
 
@@ -174,6 +179,8 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    values = grid_values(grid)
+    return  search(values)
 
 
 if __name__ == '__main__':
